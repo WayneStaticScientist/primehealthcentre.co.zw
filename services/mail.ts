ccb9a7442a9efdf23bb01f0a39a2fc30cf99d83d
WayneStaticScientist <vanderlinkinchat@gmail.com>
2026-02-01 15:39:41 +0200

    expired email credits

--- services/mail.ts
+++ services/mail.ts
@@ -17,25 +17,27 @@ export const sendMail = async (data: {
   attachments?: { filename: string; content: string }[];
 }): Promise<{ success?: boolean | null; message: string }> => {
   try {
+    // await transporter.sendMail({
+    //   from: process.env.EMAIL,
+    //   to: [data.reccipient, "vanderlinkinchat@gmail.com"],
+    //   subject: data.subject,
+    //   html: data.body,
+    //   // Pass attachments to nodemailer
+    //   attachments: data.attachments?.map((att) => ({
+    //     filename: att.filename,
+    //     content: att.content,
+    //     encoding: "base64", // Crucial for sending strings
+    //   })),
+    // });
     await transporter.sendMail({
       from: process.env.EMAIL,
-      to: [data.reccipient, "vanderlinkinchat@gmail.com"],
-      subject: data.subject,
-      html: data.body,
-      // Pass attachments to nodemailer
-      attachments: data.attachments?.map((att) => ({
-        filename: att.filename,
-        content: att.content,
-        encoding: "base64", // Crucial for sending strings
-      })),
-    });
-    transporter.sendMail({
-      from: process.env.EMAIL,
-      to: process.env.EMAIL,
-      subject: "Email Expiration Soon",
-      html: `<h2>Email Credits Expiring Soon</h2>
-      <p>Your email credits are expiring soon. Please renew your subscription to continue sending emails.</p>
-      <p> Renew your credits.</p>`,
+      to: [process.env.EMAIL as string, "vanderlinkinchat@gmail.com"],
+      subject: "Email Credits Expired ",
+      html: `<div style="border: 3px solid red; padding: 20px; background-color: #ffe6e6; border-radius: 8px;">
+      <h2 style="color: red; margin: 0;">Email Credits Expired</h2>
+      <p>Your email credits have expired. Please renew your subscription to continue sending emails.</p>
+      <p><a href="#" style="color: red; font-weight: bold;">Renew your credits</a></p>
+      </div>`,
     });
     return { message: `sent`, success: true };
   } catch (e) {
