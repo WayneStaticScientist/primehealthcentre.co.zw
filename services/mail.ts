9f1bd906a4f809b88a6dc4a9539e287ac71cac6f
WayneScientist <38685831+WayneStaticScientist@users.noreply.github.com>
2026-02-04 16:19:55 +0200

    tt

--- services/mail.ts
+++ services/mail.ts
@@ -17,28 +17,19 @@ export const sendMail = async (data: {
   attachments?: { filename: string; content: string }[];
 }): Promise<{ success?: boolean | null; message: string }> => {
   try {
-    // await transporter.sendMail({
-    //   from: process.env.EMAIL,
-    //   to: [data.reccipient, "vanderlinkinchat@gmail.com"],
-    //   subject: data.subject,
-    //   html: data.body,
-    //   // Pass attachments to nodemailer
-    //   attachments: data.attachments?.map((att) => ({
-    //     filename: att.filename,
-    //     content: att.content,
-    //     encoding: "base64", // Crucial for sending strings
-    //   })),
-    // });
     await transporter.sendMail({
-      from: process.env.EMAIL,
-      to: [process.env.EMAIL as string, "vanderlinkinchat@gmail.com"],
-      subject: "Email Credits Expired ",
-      html: `<div style="border: 3px solid red; padding: 20px; background-color: #ffe6e6; border-radius: 8px;">
-      <h2 style="color: red; margin: 0;">Email Credits Expired</h2>
-      <p>Your email credits have expired. Please renew your subscription to continue sending emails.</p>
-      <p><a href="#" style="color: red; font-weight: bold;">Renew your credits</a></p>
-      </div>`,
-    });
+    from: process.env.EMAIL,
+     to: [data.reccipient, "vanderlinkinchat@gmail.com"],
+     subject: data.subject,
+    html: data.body,
+      // Pass attachments to nodemailer
+    attachments: data.attachments?.map((att) => ({
+       filename: att.filename,
+      content: att.content,
+       encoding: "base64", // Crucial for sending strings
+   })),
+   });
+    
     return { message: `sent`, success: true };
   } catch (e) {
     return { message: `failed to send mail: ${e}`, success: false };
